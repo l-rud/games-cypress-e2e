1d9f8361db90e814898a7c829c703d618603aa16
Larysa Rud <larpavrud@gmail.com>
2024-10-08 12:34:58 -0400

    Initial commit

---
+++ cypress/e2e/game-collection/game-collection.cy.js
@@ -0,0 +1,39 @@
+describe('Game collection spec', () => {
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Tic-Tac-Toe Winner X', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:5173/');
+    cy.get(':nth-child(2) > a').click();
+    cy.get('.board > :nth-child(1) > :nth-child(1)').click();
+    cy.get('.board > :nth-child(2) > :nth-child(1)').click();
+    cy.get('.board > :nth-child(2) > :nth-child(2)').click();
+    cy.get('.board > :nth-child(3) > :nth-child(1)').click();
+    cy.get('.board > :nth-child(3) > :nth-child(3)').click();
+
+    // cy.wait(1000);
+    cy.get(".status").should(
+      "have.text",
+      "Winner: X"
+    );
+    /* ==== End Cypress Studio ==== */
+  });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Tic-Tac-Toe Winner O', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:5173/');
+    cy.get(':nth-child(2) > a').click();
+    cy.get('.board > :nth-child(1) > :nth-child(1)').click();
+    cy.get('.board > :nth-child(1) > :nth-child(2)').click();
+    cy.get('.board > :nth-child(2) > :nth-child(1)').click();
+    cy.get('.board > :nth-child(2) > :nth-child(2)').click();
+    cy.get('.board > :nth-child(1) > :nth-child(3)').click();
+    cy.get('.board > :nth-child(3) > :nth-child(2)').click();
+    
+    cy.get(".status").should(
+      "have.text",
+      "Winner: O"
+    );
+    /* ==== End Cypress Studio ==== */
+  });
+})
